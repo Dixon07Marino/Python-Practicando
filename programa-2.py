c59ed6dfa4d684aacb5e9137aa3c48e16432587a
Dixon Marino <dmusicmsoccer14@gmail.com>
2024-12-16 13:46:47 -0500

    Subiendo archivos py a el repositorio

---
+++ programa-2.py
@@ -0,0 +1,9 @@
+firstName = str("Dixon")
+b = int(7)
+c = float(9)
+last_name = 'Marino'
+
+print(type(firstName))
+print(type(b))
+print(type(c))
+print(type(last_name))
---
+++ programa-5.py
@@ -0,0 +1,7 @@
+firstName = "Dixon "
+lastName = "Marino"
+print(firstName + lastName)
+
+x = 5
+y = "7"
+print(x + y) #this will give you an error
