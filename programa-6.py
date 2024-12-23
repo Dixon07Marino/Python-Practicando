95475186e386e7885e7e6b54f454ba0d7e684f91
Dixon Marino <dmusicmsoccer14@gmail.com>
2024-12-23 10:43:45 -0500

    Repasando y aprendiendo conceptos python

---
+++ programa-6.py
@@ -0,0 +1,20 @@
+#This is a list
+name = ["Dixon", "Marino", "Eduardo", "Sanabria"]
+print(type(name))
+#This is a bool
+verdadero = True
+print(type(verdadero))
+#This is a bool
+falso = False
+print(type(falso))
+#This is a NoneType
+number = None
+print(type(number))
+
+#Especificar el tipo de dato
+fruits = list(("Apple", "Banana", "Watermelon"))
+print(fruits)
+
+numeros = bool(5)
+print(numeros)
+
