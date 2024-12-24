cbdb231b9fbfc3a445067a09f62c26392221aec4
Dixon Marino <dmusicmsoccer14@gmail.com>
2024-12-24 10:29:30 -0500

    Practicando 24/12/24

---
+++ programa-10.py
@@ -0,0 +1,35 @@
+name = "Dixon 'Marino'"
+print(name)
+
+multiline = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(multiline)
+
+# Los strings son arrays
+
+name = "Dixon Marino"
+print(name[1])
+
+fruits = ["Banana", "Pera", "Manzana"]
+print(fruits[2])
+
+fruta = "Banana"
+
+for x in fruta:
+    print(x)
+
+print(len(fruta))
+print(len(fruits))
+
+#Comprobar si un character esta dentro de un string o no
+txt = "Dixon Marino"
+
+if "Marino" in txt:
+    print("Si, la palabra 'Marino' esta dentro de txt")
+
+txt = "Dixon Marino"
+
+if "Hola" not in txt:
+    print("No, la palabra 'Hola' no esta dentro de txt")
