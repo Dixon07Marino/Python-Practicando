2b7262cc8dc3d8c7dbe7c6f3bead3c7c243423d3
Dixon Marino <dmusicmsoccer14@gmail.com>
2024-12-25 18:30:54 -0500

    same

---
+++ otrosEjercicios/programa-2.py
@@ -0,0 +1,9 @@
+text = 'Hello World'
+print(text.find("H"))
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+print(text.lower())
+index = alphabet.find(text[0].lower())
+print(index)
+shifted = alphabet[index + shift]
+print(shifted)
