3dc233b2827f7d0e25e7c3fc1bcc65d155fe4e88
Dixon Marino <dmusicmsoccer14@gmail.com>
2024-12-27 17:40:07 -0500

    Practica 27/12/24

--- otrosEjercicios/programa-2.py
+++ otrosEjercicios/programa-2.py
@@ -7,3 +7,7 @@ index = alphabet.find(text[0].lower())
 print(index)
 shifted = alphabet[index + shift]
 print(shifted)
+
+for char in text.lower():
+    index = alphabet.find(char)
+    print(char, index)
