2b6f1315a3f39106a74a1f4f7a817fc76105a5b5
Dixon Marino <dmusicmsoccer14@gmail.com>
2024-12-28 21:12:52 -0500

    28/12/24

--- mi-ejemplo.py
+++ mi-ejemplo.py
@@ -1,5 +1,16 @@
 person = ["Dixon", "Marino", 20]
-name = person[0]
+name = person[0].upper()
 last_name = person[1]
 age = person[2]
 print("Soy", name, last_name, "y tengo", age)
+
+def myFace():
+    print(":)")
+
+for name_character in name:
+    index = name.find(name_character)
+    print(name_character, index)
+    myFace()
+
+for person_info in person:
+    print(person_info)
