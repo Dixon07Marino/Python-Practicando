8dad69dd25bb6b98b4f939ca3eab7cd7aec4b507
Dixon Marino <dmusicmsoccer14@gmail.com>
2025-01-02 19:01:26 -0500

    02/01/25 aprendiendo de lists en python

--- 2/prg-1.py
+++ 2/prg-1.py
@@ -1,3 +1,14 @@
-numbers = ["Banana", "Melon", "Pera", "Durazno"]
-numbers[1:] = ["Mora", "Fresa"]
+#If you insert more items than you replace, the new items will be inserted where you specified, and the remaining items will move accordingly
+#If you insert less items than you replace, the new items will be inserted where you specified, and the remaining items will move accordingly:
+fruits = ["Banana", "Melon", "Pera"]
+fruits.insert(2, "Mango")
+print(fruits)
+
+numbers = [0, 2, 4]
+numbers.append(6)
+
+numbers_impares = [1, 3, 5]
+numbers.extend(numbers_impares)
+
 print(numbers)
+print(len(numbers))
---
+++ 2/prg-2.py
@@ -0,0 +1,15 @@
+autos = ["Tesla", "Mustan", "Ferrari", "Mustan"]
+autos.remove("Mustan")
+print(autos)
+autos.pop(2)
+print(autos)
+
+lista = [1, 2, 3, 4]
+del lista[3]
+print(lista)
+del lista
+# print(lista)
+
+lista = [1, 2, 3, 4]
+lista.clear()
+print(lista)
