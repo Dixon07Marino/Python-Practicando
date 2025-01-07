eba8ea9ef1c133c722f01a0dd24939f3e111c3c5
Dixon Marino <dmusicmsoccer14@gmail.com>
2025-01-07 14:48:28 -0500

    xd

--- 2/cs50.py
+++ 2/cs50.py
@@ -1,3 +1,7 @@
-name = input("Hola, coloca tu nombre: ").strip().title()
-name1, name2 = name.split(" ")
-print(f"Tu nombre es:\n{name1}\n{name2}")
+# name = input("Hola, coloca tu nombre: ").strip().title()
+# name1, name2 = name.split(" ")
+# print(f"Tu nombre es:\n{name1}\n{name2}")
+
+number = float(input("Ingresa el número que vas a redondear: "))
+number_redondeado = round(number, 3)
+print(number_redondeado)
---
+++ 2/prg-6.py
@@ -0,0 +1,25 @@
+sistemas = ["Windows", "Linux", "MacOS"]
+sistemas_operativos = sistemas.copy() #list(sistemas) or  sistemas[:]
+print(sistemas_operativos)
+
+numeros = [1, 2, 3, 4, 5]
+numeros_dos = [6, 7, 8, 9, 10]
+lista_numeros = numeros + numeros_dos
+print(lista_numeros)
+
+nombres = ["Dixon", "Marino", "Eduardo"]
+print(nombres.index("Marino"))
+nombres_nuevos = ["Sanabria", "Flores", "Ortiz"]
+
+for nombre in nombres_nuevos:
+    nombres.append(nombre)
+
+print(nombres)
+
+numeros_impares = [1, 3, 5, 7, 9, 9, 9]
+numeros_pares = [0, 2, 4, 6, 8]
+
+numeros_pares.extend(numeros_impares)
+print(numeros_pares)
+total = numeros_pares.count(9)
+print(total)
